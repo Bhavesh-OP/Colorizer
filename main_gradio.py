77912714e5a65c964438121ad2b161224cef72b8
Bhavesh Sinha <bhavesh03sinha@gmail.com>
2025-02-01 21:11:41 +0530

    main_gradio.py

--- main_gradio.py
+++ main_gradio.py
@@ -1,32 +1,6 @@
-import os
-import requests
 import numpy as np
 import cv2
 import gradio as gr
-
-# Define the URL and the destination path
-url = "https://www.dropbox.com/s/dx0qvhhp5hbcx7z/colorization_release_v2.caffemodel?dl=1"
-destination_folder = "models"
-destination_file = os.path.join(destination_folder, "colorization_release_v2.caffemodel")
-
-# Create the directory if it doesn't exist
-os.makedirs(destination_folder, exist_ok=True)
-
-try:
-    # Download the file
-    print("Downloading model...")
-    response = requests.get(url, stream=True)
-    response.raise_for_status()  # Raise an error for failed downloads
-
-    # Save the file
-    with open(destination_file, "wb") as file:
-        for chunk in response.iter_content(chunk_size=8192):
-            file.write(chunk)
-
-    print(f"Download completed: {destination_file}")
-
-except requests.exceptions.RequestException as e:
-    print(f"Download failed: {e}")
 
 def colorize_image(image):
     prototxt_path = 'models/colorization_deploy_v2.prototxt'
